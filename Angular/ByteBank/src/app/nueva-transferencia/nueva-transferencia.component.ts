510fa7be762a2eee854768de27ca1615185f26b0
Andres <andres.bozzani@gmail.com>
2023-07-19 19:48:16 -0300

    prueba

--- Angular/ByteBank/src/app/nueva-transferencia/nueva-transferencia.component.ts
+++ Angular/ByteBank/src/app/nueva-transferencia/nueva-transferencia.component.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./nueva-transferencia.component.scss']
 })
 export class NuevaTransferenciaComponent {
+  valor: string = "";
+  destino: string ="";
   transferir(){
     console.log("Realizar transferencia")
+    console.log("Valor: ", this.valor)
+    console.log("Cuenta: ", this.destino)
   }
 }
