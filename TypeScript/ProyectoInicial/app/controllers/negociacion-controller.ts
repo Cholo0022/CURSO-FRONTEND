e79178af22a97d0f2561164d05997d8f40386407
Cholo0022 <andres.bozzani@gmail.com>
2023-07-31 14:56:38 -0300

    Curso TypeScript modulo 3

--- TypeScript/ProyectoInicial/app/controllers/negociacion-controller.ts
+++ TypeScript/ProyectoInicial/app/controllers/negociacion-controller.ts
@@ -1,21 +1,40 @@
-import { Negociacion } from "../models/negociacion";
+import { Negociacion } from "../models/negociacion.js";
 
-export class NegociacionController{
-    private inputFecha: Date;
-    private inputCantidad: Number;
-    private inputValor: Number;
+export class NegociacionController {
+    private inputFecha: HTMLInputElement;
+    private inputCantidad: HTMLInputElement;
+    private inputValor: HTMLInputElement;
 
-    constructor(){
-        this.inputFecha = document.querySelector("#fecha").value;
-        this.inputCantidad = document.querySelector("#cantidad").value;
-        this.inputValor = document.querySelector("#valor").value;
+    constructor() {
+        this.inputFecha = document.querySelector('#fecha');
+        this.inputCantidad = document.querySelector('#cantidad');
+        this.inputValor = document.querySelector('#valor');
     }
 
-    agregar(){
-        const negociacion = new Negociacion(
-            this.inputFecha,
-            this.inputCantidad,
-            this.inputValor
-        )
+    agrega() : void {
+        const negociacion = this.crearNegociacion();
+        console.log(negociacion);
+        this.limpiaFormulario();
     }
+
+    crearNegociacion() : Negociacion {
+        const regexp = /-/g;
+        const fecha = this.inputFecha.value.replace(regexp,',');
+        const cantidad = parseInt(this.inputCantidad.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacion(
+            new Date(fecha),
+            cantidad,
+            valor
+        );
+    }
+
+    limpiaFormulario() : void {
+        this.inputFecha.value = '';
+        this.inputCantidad.value = '';
+        this.inputValor.value = '';
+        this.inputFecha.focus();
+    }
+
+    
 }
