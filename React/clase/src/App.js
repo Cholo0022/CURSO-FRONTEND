8d9be7f1f34502a491a7f5676a3fd32df36cf85a
Cholo0022 <andres.bozzani@gmail.com>
2023-08-02 14:24:21 -0300

    Proyecto final del curso React

--- React/clase/src/App.js
+++ React/clase/src/App.js
@@ -1,17 +1,35 @@
-import React, { Component } from 'react';
-import RegistroNotas from './components/RegistroNotas'
-import ListaNotas from './components/ListaNotas';
+import { Component } from "react";
+import ListaNotas from "./components/listaNotas";
+import RegistroNotas from "./components/registroNotas";
+import "./assets/index.css";
+import "./assets/App.css";
 
 class App extends Component {
-  state = {  } 
-  render() { 
+
+  constructor(){
+    super();
+    this.state = {
+      notas: []
+    };
+  }
+
+  registrar(titulo, nota){
+    const objeto = {titulo, nota};
+    const nuevoArray = [...this.state.notas, objeto];
+    const state = {
+      notas: nuevoArray
+    }
+    this.setState(state);
+  }
+
+  render() {
     return (
-      <section>
-        <RegistroNotas/>
-        <ListaNotas/>
+      <section className="contenido">
+        <RegistroNotas registrar={this.registrar.bind(this)} />
+        <ListaNotas notas={this.state.notas} />
       </section>
     );
   }
 }
- 
+
 export default App;
